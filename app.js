41a8c62b5c360c41222258311a193cf594ce8b08
Alexandre OBELLIANNE <aobellianne@outlook.com>
2019-05-21 15:17:01 +0800

    Degub

--- app.js
+++ app.js
@@ -1,19 +1,38 @@
-const http = require('http');
+const express = require("express");
+const app = express();
+const basicAuth = require("express-basic-auth");
 
-const port = process.env.PORT || 3000
+app.use([
+    basicAuth({
+        challenge: true,
+        users: { palo_ocbc: "ocbc_palo" },
+    }),
+    express.static("./src/"),
+]);
 
-const server = http.createServer((req, res) => {
-
-    res.statusCode = 200;
-
-    res.setHeader('Content-Type', 'text/html');
-
-    res.end('<h1>Hello World yo!</h1>');
-
+app.listen(process.env.PORT || 3000, function() {
+    console.log("Example app listening on port 3000!");
 });
 
-server.listen(port, () => {
 
-    console.log(`Server running at port ` + port);
 
-});
+
+// const http = require('http');
+
+// const port = process.env.PORT || 3000
+
+// const server = http.createServer((req, res) => {
+
+//     res.statusCode = 200;
+
+//     res.setHeader('Content-Type', 'text/html');
+
+//     res.end('<h1>Hello World</h1>');
+
+// });
+
+// server.listen(port, () => {
+
+//     console.log(`Server running at port ` + port);
+
+// });
